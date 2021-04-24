cd813de9071134b949fb6d498e6687cbbb10d2a5
hectorvictor <victohector@gmail.com>
2021-04-23 22:50:43 -0300

    GET / orders

--- back/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ back/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.devsuperior.dsdeliver.repositories;
 
+import java.util.List;
+
 import com.devsuperior.dsdeliver.entity.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findAllByOrderByAddressAsc();
     
 }
