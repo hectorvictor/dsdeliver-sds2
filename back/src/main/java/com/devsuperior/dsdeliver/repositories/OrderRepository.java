43bea8109b4e03b4cae8a1b1c100c576212ef884
hectorvictor <victohector@gmail.com>
2021-04-24 14:30:31 -0300

    GET/ orders peding

--- back/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ back/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -5,11 +5,13 @@ import java.util.List;
 import com.devsuperior.dsdeliver.entity.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    List<Order> findAllByOrderByAddressAsc();
+    @Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products WHERE obj.status = 0 ORDER BY obj.moment ASC")
+    List<Order> findOrdersWithProducts();
     
 }
