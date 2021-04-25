d195b788917ecf01ec0eab45973c303dd1bf4939
hectorvictor <victohector@gmail.com>
2021-04-25 11:46:22 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 
 function App() {
   return (
-    <div>
-        
-    </div>
+    <>
+          <Navbar/>
+          <Home/>
+    </>
   );
 }
 
